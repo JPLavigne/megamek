91cdf9e25850a71b7328ab72d5b9ef2c9fdd1282
Jean-Pascal Lavigne <jean-pascal.lavigne@live.ca>
2022-04-06 08:54:56 -0400

    Creation de la classe standings

---
+++ megamek/src/megamek/server/Standings.java
@@ -0,0 +1,15 @@
+package megamek.server;
+
+public class Standings {
+    public static void eloCalculation(){
+
+    }
+
+    public static void updatePlayerEloScrore(){
+
+    }
+
+    public static void updateStandings(){
+
+    }
+}
